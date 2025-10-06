edd4ab45e3250a213a915f2bda2b4c0043cf5eeb
Vietanh21082004 <vietle21082004@gmail.com>
2025-10-06 20:44:51 +0700

    Tao class book

---
+++ Backend/QuanLyThuVien/Model/book.cs
@@ -0,0 +1,14 @@
+﻿namespace baiapi1.Models
+{
+    public class book
+    {
+        public int BookId { get; set; } 
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public int PublicationYear { get; set; }
+        public string Image {  get; set; }
+    }
+}
