aad200b5b2136c7deb120140700e036d36270ba7
Vietanh21082004 <vietle21082004@gmail.com>
2025-10-06 21:05:13 +0700

    Tao interface book

---
+++ Backend/QuanLyThuVien/DLL/Interfaces/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using baiapi1.DTO;
+using baiapi1.Models;
+using Microsoft.VisualBasic;
+
+namespace baiapi1.DAL.Interfaces
+{
+    public interface IBookRepository
+    {
+        List<book> GetAll();
+        book GetById(int id);
+        List<book> GetByCate(string TheLoai);
+
+        book ten(string sach);
+        List<book> tensach(int nam);
+
+        bool BorrowBook(BorrowBookDTO dto);
+
+        List<LoanDTO> GetLoan(int id);
+    }
+}
